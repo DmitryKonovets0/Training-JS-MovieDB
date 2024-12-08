e56ff4d6dc2871f3857fca09ada51bd9fd41f532
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-12-08 20:20:40 +0100

    day 9 - trainingJS - MovieDB

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+/* Задание на урок:
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-3) Изменить задний фон с постером фильма на изображение "bg.jpg". Оно лежит в папке img.
-    Реализовать только при помощи JS
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-    Отсортировать их по алфавиту
-
-5) Добавить нумерацию выведенных фильмов */
-
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку.
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены -
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    rememberMyFilms: () => {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его?', '');
+
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }}},
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }},
+    showMyDB: (hidden) => {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }},
+    writeYourGenres: () => {
+        for (let i = 1; i <= 3; i++) {
+            let genres = personalMovieDB.genres
+            if (genres != null && genres != ''  && genres.length < 50) {
+                genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+            }
+        }
+    },
+    toggleVisibleMyDB: () => {
+        if( personalMovieDB.privat ) {
+            personalMovieDB.privat.toggle(personalMovieDB.privat);
+        }
+    }
 };
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
 
-adv.forEach(item => {
-    item.remove();
-});
-
-genre.textContent = 'драма';
-
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-
-movieList.innerHTML = "";
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel()
+personalMovieDB.showMyDB(personalMovieDB.privat)
+personalMovieDB.writeYourGenres()
+personalMovieDB.toggleVisibleMyDB()
+console.log(personalMovieDB);
